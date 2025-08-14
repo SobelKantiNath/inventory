a6d081d36d6c15b5671740493092f6a81aca8dfd
sobel <sobelkantinath@gmail.com>
2025-08-14 22:45:54 +0600

    After Setup Admin Panel

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->string('role')->default('user')->nullable();
+            $table->string('status')->default('1')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
